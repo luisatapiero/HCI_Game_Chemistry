939aa8a5d29405d83b641877f2abfd9d6df84970
luisatapiero <79235499+luisatapiero@users.noreply.github.com>
2022-10-23 11:58:11 -0500

    First level working

--- sketch.js
+++ sketch.js
@@ -1,6 +1,5 @@
 let puntaje;
 
-let pantalla2;
 let pantalla = 1;
 
 let bgCompound1;
@@ -10,6 +9,7 @@ let bgCompound4;
 let bgCompound5;
 
 let instructions;
+let notebook;
 
 let showInstructions;
 
@@ -25,12 +25,16 @@ answerList = [];
   let min = 5;
   let timer;
 
-function changeLevel() {
-    window.location.href = '/nivel-3';
+function changeLevel2() {
+    window.location.href = '/nivel-2';
 }
 
 function saveScore(score) {
     localStorage.setItem("score", score);
+}
+
+function saveTime(time) {
+    localStorage.setItem("time", time);
 }
 
 function setup() {
@@ -50,7 +54,10 @@ function setup() {
     bgCompound3 = loadImage("./img/Carbomero.png");
     bgCompound4 = loadImage("./img/Trietanolamina.png");
     bgCompound5 = loadImage("./img/Alcohol.png");
-    instructions = loadImage("./img/cuadernoIns.png");
+    instructions = loadImage("./img/instrucciones.png");
+    notebook = loadImage("./img/cuadernoIns.png");
+    portada = loadImage("./img/portada.png");
+    aviso = loadImage("./img/aviso.png");
     //prueba = loadImage("./img/prueba.png");
 
     matraz = loadImage("./img/matraz.png");
@@ -65,83 +72,102 @@ function setup() {
 
 function draw() {
 
-    time();
-
-    //console.log(timer);
-
-    /*if (showInstructions == true){
-                imageMode(CORNER);
-                instructions.resize(1214, 683);
-                image(instructions, 100, 100);
-                //console.log(showInstructions);
-    }else{
-        imageMode(CORNER);
-                instructions.resize(1214, 683);
-                image(instructions, 100, 100);
-    }*/
-
+    if (level >0){
+        time();
+    }
     
 
     switch (pantalla) {
 
-
         case 1:
+
+            imageMode(CORNER);
+            background(portada);
+
+            break;
+
+        case 2:
+
+            imageMode(CORNER);
+            background(aviso);
+
+            break;
+
+        case 3:
+
+            imageMode(CORNER);
+            background(instructions);
+
+            break;
+
+        case 4:
 
             level = 1;
             imageMode(CORNER);
             background(bgCompound1);
             //if(answerList.length > 0){
-            fill(0);
-            text((getFormattedValue(answerList)), 246, 695);
+                fill(0);
+                textStyle(BOLD);
+                textSize(14);
+                text((getFormattedValue(answerList)), 400, 695);
 
 
             break;
-        case 2:
+        case 5:
             level = 2;
             imageMode(CORNER);
             background(bgCompound2);
 
             fill(0);
-            text((getFormattedValue(answerList)), 246, 695);
+            textStyle(BOLD);
+            textSize(14);
+            text((getFormattedValue(answerList)), 400, 695);
 
             break;
-        case 3:
+        case 6:
 
             level = 3;
             imageMode(CORNER);
             background(bgCompound3);
 
             fill(0);
-            text((getFormattedValue(answerList)), 246, 695);
+            textStyle(BOLD);
+            textSize(14);
+            text((getFormattedValue(answerList)), 400, 695);
 
             break;
 
-        case 4:
+        case 7:
 
             level = 4;
             imageMode(CORNER);
             background(bgCompound4);
 
             fill(0);
-            text((getFormattedValue(answerList)), 246, 695);
+            textStyle(BOLD);
+            textSize(14);
+            text((getFormattedValue(answerList)), 400, 695);
 
             break;
 
-        case 5:
+        case 8:
             level = 5;
             imageMode(CORNER);
             background(bgCompound5);
 
             fill(0);
-            text((getFormattedValue(answerList)), 246, 695);
+            textStyle(BOLD);
+            textSize(14);
+            text((getFormattedValue(answerList)), 400, 695);
 
             break;
 
-        case 6:
+        case 9:
                 imageMode(CORNER);
                 //background(bgCompound5);
-                instructions.resize(1214, 683);
-                image(instructions, 100, 50);
+                notebook.resize(1214, 683);
+                image(notebook, 100, 50);
+                puntaje -= 2;
     
                 //fill(0);
                 //text((getFormattedValue(answerList)), 246, 695);
@@ -149,7 +175,9 @@ function draw() {
         break;
     }
 
-
+    fill(255,255,255);
+    textSize(18);
+    text(timer, 105, 50);
 
 }
 
@@ -173,39 +201,55 @@ function time() {
 
 
     if (min == 0 && sec == 1) {
-        //this.calcularPuntajePorTiempo();
-        //this.nivel = 11;
+        changeLevel2();
     } 
 }
 
 
 function clearAnswerList() {
     answerList = [];
-    image(spriteGoodResult[frameCount % 15], 200, 200, 400, 400);
 }
 
 function mousePressed() {
     saveScore(100);
     //changeLevel();
 
-    if (mouseX > 801 && mouseX < 946 && mouseY > 664 && mouseY < 716) {
-        //console.log("borrar");
-        clearAnswerList();
-    }
 
-    if (mouseX > 1188 && mouseX < 1385 && mouseY > 384 && mouseY < 685) {
-        checkUserAnswer();
-        console.log(puntaje);
-
-    }
-
-    if (mouseX > 1267 && mouseX < 1381 && mouseY > 67 && mouseY < 160) {
-        pantalla = 6;
-    }
+    
 
     switch (pantalla) {
 
+        
+
         case 1:
+
+            console.log(mouseX + "," + mouseY)
+            if (mouseX > 643 && mouseX < 887 && mouseY > 600 && mouseY < 655) {
+                pantalla++;
+            }
+            
+
+            break;
+
+        case 2:
+
+            console.log(mouseX + "," + mouseY)
+            if (mouseX > 1288 && mouseX < 1481 && mouseY > 645 && mouseY < 700) {
+                pantalla++;
+            }
+
+            break;
+
+        case 3:
+            console.log(mouseX + "," + mouseY)
+            if (mouseX > 1288 && mouseX < 1481 && mouseY > 645 && mouseY < 700) {
+                pantalla++;
+            }
+
+            break;
+
+
+        case 4:
 
             console.log(mouseX + "," + mouseY)
 
@@ -223,61 +267,56 @@ function mousePressed() {
                 answerList.push('O');
             }
 
-            
 
-
-
-
-            break;
-        case 2:
-            if (mouseX > 101 && mouseX < 167 && mouseY > 410 && mouseY < 641) {
-                answerList.push('C');
+            if (mouseX > 801 && mouseX < 946 && mouseY > 664 && mouseY < 716) {
+                //console.log("borrar");
+                clearAnswerList();
             }
-            if (mouseX > 294 && mouseX < 359 && mouseY > 410 && mouseY < 641) {
-                answerList.push('H');
+        
+            if (mouseX > 1188 && mouseX < 1385 && mouseY > 384 && mouseY < 685) {
+                checkUserAnswer();
+                console.log(puntaje);
+        
             }
-            if (mouseX > 485 && mouseX < 552 && mouseY > 410 && mouseY < 641) {
-                answerList.push('N');
-            }
-            if (mouseX > 678 && mouseX < 744 && mouseY > 410 && mouseY < 641) {
-                answerList.push('O');
+        
+            if (mouseX > 1267 && mouseX < 1381 && mouseY > 67 && mouseY < 160) {
+                pantalla = 9;
             }
 
             break;
-
-        case 3:
-
-            if (mouseX > 101 && mouseX < 167 && mouseY > 410 && mouseY < 641) {
-                answerList.push('C');
-            }
-            if (mouseX > 294 && mouseX < 359 && mouseY > 410 && mouseY < 641) {
-                answerList.push('H');
-            }
-            if (mouseX > 485 && mouseX < 552 && mouseY > 410 && mouseY < 641) {
-                answerList.push('N');
-            }
-            if (mouseX > 678 && mouseX < 744 && mouseY > 410 && mouseY < 641) {
-                answerList.push('O');
-            }
-            break;
-
-        case 4:
-
-            if (mouseX > 101 && mouseX < 167 && mouseY > 410 && mouseY < 641) {
-                answerList.push('C');
-            }
-            if (mouseX > 294 && mouseX < 359 && mouseY > 410 && mouseY < 641) {
-                answerList.push('H');
-            }
-            if (mouseX > 485 && mouseX < 552 && mouseY > 410 && mouseY < 641) {
-                answerList.push('N');
-            }
-            if (mouseX > 678 && mouseX < 744 && mouseY > 410 && mouseY < 641) {
-                answerList.push('O');
-            }
-            break;
-
         case 5:
+            if (mouseX > 101 && mouseX < 167 && mouseY > 410 && mouseY < 641) {
+                answerList.push('C');
+            }
+            if (mouseX > 294 && mouseX < 359 && mouseY > 410 && mouseY < 641) {
+                answerList.push('H');
+            }
+            if (mouseX > 485 && mouseX < 552 && mouseY > 410 && mouseY < 641) {
+                answerList.push('N');
+            }
+            if (mouseX > 678 && mouseX < 744 && mouseY > 410 && mouseY < 641) {
+                answerList.push('O');
+            }
+
+
+            if (mouseX > 801 && mouseX < 946 && mouseY > 664 && mouseY < 716) {
+                //console.log("borrar");
+                clearAnswerList();
+            }
+        
+            if (mouseX > 1188 && mouseX < 1385 && mouseY > 384 && mouseY < 685) {
+                checkUserAnswer();
+                console.log(puntaje);
+        
+            }
+        
+            if (mouseX > 1267 && mouseX < 1381 && mouseY > 67 && mouseY < 160) {
+                pantalla = 9;
+            }
+
+            break;
+
+        case 6:
 
             if (mouseX > 101 && mouseX < 167 && mouseY > 410 && mouseY < 641) {
                 answerList.push('C');
@@ -291,13 +330,93 @@ function mousePressed() {
             if (mouseX > 678 && mouseX < 744 && mouseY > 410 && mouseY < 641) {
                 answerList.push('O');
             }
+
+            if (mouseX > 801 && mouseX < 946 && mouseY > 664 && mouseY < 716) {
+                //console.log("borrar");
+                clearAnswerList();
+            }
+        
+            if (mouseX > 1188 && mouseX < 1385 && mouseY > 384 && mouseY < 685) {
+                checkUserAnswer();
+                console.log(puntaje);
+        
+            }
+        
+            if (mouseX > 1267 && mouseX < 1381 && mouseY > 67 && mouseY < 160) {
+                pantalla = 9;
+            }
+
             break;
 
-            case 6:
+        case 7:
+
+            if (mouseX > 101 && mouseX < 167 && mouseY > 410 && mouseY < 641) {
+                answerList.push('C');
+            }
+            if (mouseX > 294 && mouseX < 359 && mouseY > 410 && mouseY < 641) {
+                answerList.push('H');
+            }
+            if (mouseX > 485 && mouseX < 552 && mouseY > 410 && mouseY < 641) {
+                answerList.push('N');
+            }
+            if (mouseX > 678 && mouseX < 744 && mouseY > 410 && mouseY < 641) {
+                answerList.push('O');
+            }
+
+            if (mouseX > 801 && mouseX < 946 && mouseY > 664 && mouseY < 716) {
+                //console.log("borrar");
+                clearAnswerList();
+            }
+        
+            if (mouseX > 1188 && mouseX < 1385 && mouseY > 384 && mouseY < 685) {
+                checkUserAnswer();
+                console.log(puntaje);
+        
+            }
+        
+            if (mouseX > 1267 && mouseX < 1381 && mouseY > 67 && mouseY < 160) {
+                pantalla = 9;
+            }
+
+            break;
+
+        case 8:
+
+            if (mouseX > 101 && mouseX < 167 && mouseY > 410 && mouseY < 641) {
+                answerList.push('C');
+            }
+            if (mouseX > 294 && mouseX < 359 && mouseY > 410 && mouseY < 641) {
+                answerList.push('H');
+            }
+            if (mouseX > 485 && mouseX < 552 && mouseY > 410 && mouseY < 641) {
+                answerList.push('N');
+            }
+            if (mouseX > 678 && mouseX < 744 && mouseY > 410 && mouseY < 641) {
+                answerList.push('O');
+            }
+
+            if (mouseX > 801 && mouseX < 946 && mouseY > 664 && mouseY < 716) {
+                //console.log("borrar");
+                clearAnswerList();
+            }
+        
+            if (mouseX > 1188 && mouseX < 1385 && mouseY > 384 && mouseY < 685) {
+                checkUserAnswer();
+                console.log(puntaje);
+        
+            }
+        
+            if (mouseX > 1267 && mouseX < 1381 && mouseY > 67 && mouseY < 160) {
+                pantalla = 9;
+            }
+
+            break;
+
+            case 9:
                 console.log(mouseX + "," + mouseY)
             if (mouseX > 196 && mouseX < 240 && mouseY > 117 && mouseY < 159) {
                 //answerList.push('C');
-                pantalla = level;
+                pantalla = level+3;
             }
             
             break;
@@ -309,60 +428,62 @@ function mousePressed() {
 
 function checkUserAnswer() {
     switch (pantalla) {
-        case 1:
+        case 4:
 
             if (getFormattedValue(answerList) == 'H2O') {
                 puntaje += 10;
                 pantalla++;
             } else {
-                puntaje -= 2;
+                puntaje -= 5;
             }
             clearAnswerList();
 
             break;
-        case 2:
+        case 5:
 
             if (getFormattedValue(answerList) == 'C3H8O3') {
                 puntaje += 10;
                 pantalla++;
             } else {
-                puntaje -= 2;
+                puntaje -= 5;
             }
             clearAnswerList();
 
             break;
 
-        case 3:
+        case 6:
 
-            if (getFormattedValue(answerList) == 'C3H8O3') {
+            if (getFormattedValue(answerList) == 'C3H4O2') {
                 puntaje += 10;
                 pantalla++;
             } else {
-                puntaje -= 2;
+                puntaje -= 5;
             }
             clearAnswerList();
 
             break;
 
-        case 4:
+        case 7:
 
             if (getFormattedValue(answerList) == 'C6H15NO3') {
                 puntaje += 10;
                 pantalla++;
             } else {
-                puntaje -= 2;
+                puntaje -= 5;
             }
             clearAnswerList();
 
             break;
 
-        case 5:
+        case 8:
 
-            if (getFormattedValue(answerList) == 'C2H5NO3') {
+            if (getFormattedValue(answerList) == 'C2H5OH') {
                 puntaje += 10;
-                pantalla++;
+                saveScore(puntaje);
+                saveTime(timer);
+                changeLevel2();
             } else {
-                puntaje -= 2;
+                puntaje -= 5;
             }
             clearAnswerList();
 
@@ -388,14 +509,4 @@ function getFormattedValue(array) {
     }, '');
 
     return reduced.toUpperCase();
-}
-
-function mouseDragged() {
-
-
-}
-
-function mouseReleased() {
-
-
 }
